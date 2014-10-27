ed92805cd9fb6f8c15f5ffd00e133283b64272f1
09oguri <oguri.h.aa@m.titech.ac.jp>
2014-10-28 00:08:06 +0900

    add importer

---
+++ scripts/dbpedia-importer.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "setup DBpedia Japanese"
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://ja.dbpedia.org/resource/%E6%9D%B1%E4%BA%AC%E5%9B%BD%E7%AB%8B%E5%8D%9A%E7%89%A9%E9%A4%A8 lod/dbpedia-ttl/東京国立博物館.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://ja.dbpedia.org/resource/%E4%BA%AC%E9%83%BD%E5%A4%A7%E5%AD%A6%E6%96%87%E5%AD%A6%E7%A0%94%E7%A9%B6%E7%A7%91%E5%9B%B3%E6%9B%B8%E9%A4%A8 lod/dbpedia-ttl/京都大学文学研究科図書館.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://ja.dbpedia.org/resource/%E5%A4%A7%E9%98%AA%E5%B8%82%E7%AB%8B%E7%BE%8E%E8%A1%93%E9%A4%A8 lod/dbpedia-ttl/大阪市立美術館.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data "http://ja.dbpedia.org/resource/%E5%B1%B1%E7%A8%AE%E7%BE%8E%E8%A1%93%E9%A4%A8(%E5%B1%B1%E7%A8%AE%E7%BE%8E%E8%A1%93%E8%B2%A1%E5%9B%A3)" "lod/dbpedia-ttl/山種美術館(山種美術財団).ttl"
---
+++ scripts/lodac-importer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "setup LODAC"
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/904 lod/lodac-ttl/904.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/1626 lod/lodac-ttl/1626.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/3157 lod/lodac-ttl/3157.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/3162 lod/lodac-ttl/3162.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/3400 lod/lodac-ttl/3400.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/3585 lod/lodac-ttl/3585.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/3786 lod/lodac-ttl/3786.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/4382 lod/lodac-ttl/4382.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/4405 lod/lodac-ttl/4405.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/5289 lod/lodac-ttl/5289.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/5643 lod/lodac-ttl/5643.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/5749 lod/lodac-ttl/5749.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/6373 lod/lodac-ttl/6373.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/7311 lod/lodac-ttl/7311.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/7312 lod/lodac-ttl/7312.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/81310 lod/lodac-ttl/81310.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/81391 lod/lodac-ttl/81391.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/83095 lod/lodac-ttl/83095.ttl
+./jena-fuseki-1.1.1/s-put http://localhost:3030/data/data http://lod.ac/id/486900 lod/lodac-ttl/486900.ttl
